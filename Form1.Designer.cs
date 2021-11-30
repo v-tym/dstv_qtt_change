0fa46860dc79e5a317d510e1c8a6707b1829f869
Vitaliy Tymoshenko <95086832+v-tym@users.noreply.github.com>
2021-11-30 17:22:32 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿
+namespace dstv_qtt_change
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.openExel = new System.Windows.Forms.Button();
+            this.pathToDSTV = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.Start = new System.Windows.Forms.Button();
+            this.messages = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(563, 30);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Путь к ексель файлу";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(13, 77);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(563, 30);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Text = "Путь к папке дств";
+            // 
+            // openExel
+            // 
+            this.openExel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.openExel.Location = new System.Drawing.Point(582, 6);
+            this.openExel.Name = "openExel";
+            this.openExel.Size = new System.Drawing.Size(201, 40);
+            this.openExel.TabIndex = 2;
+            this.openExel.Text = "Open excel";
+            this.openExel.UseVisualStyleBackColor = true;
+            this.openExel.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pathToDSTV
+            // 
+            this.pathToDSTV.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pathToDSTV.Location = new System.Drawing.Point(582, 71);
+            this.pathToDSTV.Name = "pathToDSTV";
+            this.pathToDSTV.Size = new System.Drawing.Size(201, 38);
+            this.pathToDSTV.TabIndex = 4;
+            this.pathToDSTV.Text = "Open path to DSTV";
+            this.pathToDSTV.UseVisualStyleBackColor = true;
+            this.pathToDSTV.Click += new System.EventHandler(this.pathToDSTV_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Start
+            // 
+            this.Start.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Start.Location = new System.Drawing.Point(13, 143);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(770, 45);
+            this.Start.TabIndex = 5;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // messages
+            // 
+            this.messages.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.messages.Location = new System.Drawing.Point(13, 216);
+            this.messages.Multiline = true;
+            this.messages.Name = "messages";
+            this.messages.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.messages.Size = new System.Drawing.Size(770, 174);
+            this.messages.TabIndex = 6;
+            this.messages.Text = "Сообщения ошибок...";
+            this.messages.TextChanged += new System.EventHandler(this.messages_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(795, 402);
+            this.Controls.Add(this.messages);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.pathToDSTV);
+            this.Controls.Add(this.openExel);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button openExel;
+        private System.Windows.Forms.Button pathToDSTV;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.TextBox messages;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
+
+namespace dstv_qtt_change
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            openFileDialog1.Filter = "xls files(*.xls|*.xls|*.xlsx|*.xlsx|All files(*.*)|*.*";
+        }
+
+        private void button1_Click(object sender, EventArgs e) //openExel
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            
+            string filename = openFileDialog1.FileName;// получаем выбранный файл            
+            textBox1.Text = filename; //путь файла в тестовое поле           
+        }
+
+        private void pathToDSTV_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            string filename = folderBrowserDialog1.SelectedPath;
+            textBox2.Text = filename; //путь файла в тестовое поле   
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Dictionary<string, int> Pos = GetDictionaryOfPartsNonOffice(textBox1.Text);
+                //Dictionary<int, int> Pos1 = GetDictionaryOfPartsOfice(textBox1.Text);
+                  //GetAllDSTV(textBox2.Text);
+                 changeDSTV(Pos, GetAllDSTV(textBox2.Text));
+                 //MessageBox.Show("Выполнение завершено");
+            }
+            catch (Exception ex)
+            {
+                messages.Text ="Error: " + ex.Source +  ex.Message;
+            }
+                    
+        }
+        private Dictionary<string, int> GetDictionaryOfPartsNonOffice(string pathToExcel)
+        {
+            FileStream xlsFile = new FileStream(pathToExcel, FileMode.Open, FileAccess.Read);
+            HSSFWorkbook hssfwb = new HSSFWorkbook(xlsFile);
+            //ISheet sheet1 = hssfwb.GetSheet("_5_Список деталей");
+            HSSFSheet sheet = (HSSFSheet)hssfwb.GetSheetAt(0);
+            Dictionary<string, int> PositionQtt = new Dictionary<string, int>();
+            for (int row = 3; row <= (sheet.LastRowNum-1); row++)
+            {
+                //получаем строку
+                string position = sheet.GetRow(row).Cells[1].ToString();
+                int qtt = Convert.ToInt32(sheet.GetRow(row).Cells[3].ToString());
+                PositionQtt.Add(position, qtt);
+            }
+            return PositionQtt;
+
+        }
+        private Dictionary<int, int> GetDictionaryOfPartsOfice(string pathToExcel)
+        {
+           // int startLine = 4; // начинаем считывает со строки 4(в ексель А=4,В=4... и т.д.)
+           // char column1 = 'B'; //считываемый столбец
+           // char column2 = 'D'; //считываемый столбец
+
+            Dictionary<int, int> PositionQtt = new Dictionary<int, int>();
+
+            Excel.Application ObjWorkExcel = new Excel.Application(); //открыть эксель
+            Excel.Workbook ObjWorkBook = ObjWorkExcel.Workbooks.Open(@pathToExcel, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing); //открыть файл
+            Excel.Worksheet ObjWorkSheet = (Excel.Worksheet)ObjWorkBook.Sheets[1]; //получить 1 лист
+            //var lastCell = ObjWorkSheet.Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell);
+
+            //int iLastRow = ObjWorkSheet.Cells[ObjWorkSheet.Rows.Count, "A"].End[Excel.XlDirection.xlUp].Row;  //последняя заполненная строка в столбце А
+            //var arrData = (object[,])ObjWorkSheet.Range["B:D" + iLastRow].Value;
+            //var x = ObjWorkSheet.Range["B"];
+
+            Excel.Range columnB = ObjWorkSheet.UsedRange.Columns[2];
+            Excel.Range columnD = ObjWorkSheet.UsedRange.Columns[4];
+            System.Array valB = (System.Array)columnB.Cells.Value;
+            System.Array valD = (System.Array)columnD.Cells.Value;
+            string[] position = valB.OfType<object>().Select(o => o.ToString()).ToArray();
+            string[] qtt = valD.OfType<object>().Select(o => o.ToString()).ToArray();
+            //Удаляем текстовое поле
+            position = position.Where(val => val != "Позиція").ToArray();
+            qtt = qtt.Where(val => val != "Кількість, шт").ToArray();
+            for (int i = 0; i < position.Length; i++)
+            {
+                PositionQtt.Add(Convert.ToInt32(position[i]), Convert.ToInt32(qtt[i]));
+            }
+
+            ObjWorkBook.Close(false, Type.Missing, Type.Missing); //закрыть не сохраняя
+            ObjWorkExcel.Quit(); // выйти из экселя
+
+            return PositionQtt;
+        }
+
+        private string[] GetAllDSTV(string path)
+        {
+            string[] files = Directory.GetFiles($@"{path}", "*.nc1", SearchOption.AllDirectories);
+            //this.listBox1.Items.AddRange(files);
+            //int x = 34;
+            return files;
+        }
+
+        private void changeDSTV(Dictionary<string, int> excelDatas, string[] pathsToDSTV)
+        {
+            List<string> messagesErr = new List<string>();
+            messagesErr.Add("Выполнение завершено");
+           foreach (KeyValuePair <string, int> key in excelDatas)
+            {
+                if (searchFile(key.Key, pathsToDSTV))
+                {
+                    IEnumerable<string> pathToDSTV = pathsToDSTV.Where(val => val == (Path.GetDirectoryName(val) +"\\" + $"{key.Key}.nc1"));
+                   if(pathToDSTV.ToArray().Length == 1)
+                    {
+                        string[] path = pathToDSTV.ToArray();
+                        changeQttNC(path[0], key.Value);
+                    }
+                   else
+                    {
+                        string[] paths = pathToDSTV.ToArray();
+                        messagesErr.Add("Одинаковый номер дств в разных папках...");
+                        foreach (string val in paths)
+                        {
+                            messagesErr.Add(val);
+                        }
+                        
+                    }
+                }
+                else
+                {
+                    messagesErr.Add("Файл дств не найден:" + key.Key);
+
+                }
+            }
+            string[] messagesErrRes = messagesErr.ToArray();
+            string messER = String.Join("\r\n", messagesErrRes);
+
+            messages.Text = messER;
+        }
+
+        private bool searchFile(string position, string[] pathsToDSTV)
+        {            
+             foreach (string path in pathsToDSTV)
+             {
+                string dstv = Path.GetDirectoryName(path) + "\\" + $"{position}.nc1";
+                // string[] words = path.Split(new char[] { '\\' });
+                // string dstvFileName = words[words.Length-1];
+                if (dstv == path)
+                 {
+                     return true;
+                 }                 
+             }
+            return false;            
+            /*foreach (string path in pathsToDSTV)
+            {
+                var x = Path.GetDirectoryName(path)+"\\" + $"{position}.nc1";
+            }*/
+            //IEnumerable<string> result = pathsToDSTV.Where(val => val == (Path.GetDirectoryName(val) +"\\" + $"{position}.nc1"));
+           
+        }    
+        
+        private void changeQttNC (string pathToDSTV, int qtt)
+        {
+            try
+            {
+                StreamReader sr = new StreamReader(pathToDSTV, System.Text.Encoding.Default);
+                var datasSR = sr.ReadToEnd();
+                string[] words = datasSR.Split(new char[] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
+                int qttStrFromDstv = Convert.ToInt32((words[7].Trim(new char[] { ' ', '\r' })));    
+                if (qtt != qttStrFromDstv) 
+                {
+                    words[7] = "  " + qtt + "\r";
+                }
+                else
+                {
+                    return;
+                }
+
+                for (int i = 0; i<words.Length; i++)
+                {
+                    words[i] = words[i] + '\n';
+                }
+                sr.Close();
+                ///перезапись файла дств
+                string newDstvData = String.Join("", words);
+                StreamWriter sw = new StreamWriter(pathToDSTV, false, System.Text.Encoding.Default);
+                sw.WriteLine(newDstvData);
+                sw.Close();
+
+
+
+            }
+            catch (Exception e)
+            {
+
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void messages_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
